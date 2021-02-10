52bdfdd8431c411a5f4469e4973ac67cc8417d07
nagraja <nagmothukuri@gmail.com>
2021-02-10 17:17:11 -0500

    Updated to default browser type to chrome

--- first_project/src/test/SignIn_Script1.java
+++ first_project/src/test/SignIn_Script1.java
@@ -17,16 +17,16 @@ public class SignIn_Script1
 	//ReadTestCasesData rtcdOBJ = new ReadTestCasesData();
 			
 	//Calling and Assigning Returned Value from Browser TypeFunction
-	String Selected_BT = gfOBJ.BrowserTypeRequest_Fun();
+	//String Selected_BT = gfOBJ.BrowserTypeRequest_Fun();
 	
 	//Calling Browser Launch Function and Assigning Driver value returned from Function
-	WebDriver DriverOBJ = gfOBJ.LaunchBrowser_Fun(Selected_BT);
+	WebDriver DriverOBJ = gfOBJ.LaunchBrowser_Fun("Chrome");
 			
 	gfOBJ.LaunchApplication_Fun();
 	String URL_Value = DriverOBJ.getCurrentUrl();
 	System.out.println("Browser Launched"+URL_Value);
-	System.out.println("What Browser Selected Is: "+Selected_BT+" and Launched"+URL_Value);
-	
+	//System.out.println("What Browser Selected Is: "+Selected_BT+" and Launched"+URL_Value);
+		
 	
 	//Function Library and Object Repository Object Creation
 	app_fl_or florOBJ = new app_fl_or(DriverOBJ);
